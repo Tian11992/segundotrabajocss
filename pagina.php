f739ae1cc098b93c35470583ee75893033ccfd0e
Sebastian Correa <sebas.11992@hotmail.com>
2019-11-30 13:55:35 -0500

    segundo trabajo introduccion php

---
+++ pagina.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <title>
+            Coca-Cola
+        </title>
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+        <div id="pagewrap">
+                <img id="logo1" src="images/cocacola.png">
+                <img id="logo2" src="images/cocacola.png">
+    <center>
+        <div id="header">
+
+                <h1>Coca-Cola</h1>
+                <p>Inicio - Mision - Vision - Contactenos</p>
+                
+
+        </div>
+    </center>
+<div id="content">
+            <h2>Historia</h2>
+            <p>Un 8 de mayo de 1886 comenzaba la historia de Coca-Cola en Atlanta. El farmacéutico John S. Pemberton 
+            quería crear un jarabe contra los problemas de digestión que además aportase energía, y acabó dando con 
+            la bebida más famosa del mundo. La farmacia Jacobs fue la primera en comercializar el preparado a un precio 
+            de 5 centavos el vaso, vendiendo unos nueve cada día. Era solo el inicio de una historia de más de 130 años.</p>
+        
+            <h2>Celebra 130 años</h2>
+            <p>Han pasado 130 años desde que aquella bebida novedosa se sirviera por primera vez en una fuente de sodas en Atlanta, 
+            Georgia. Hoy en día, el tomar una Coca-Cola implica no sólo tener entre las manos al refresco favorito del mundo, 
+            sino toda una historia de esfuerzo y dedicación, un presente lleno de entusiasmo, trabajo y energía, y un porvenir 
+            sólido impulsado por el compromiso de miles de empleados que constituyen el sistema Coca-Cola en cada país del orbe</p>
+
+        <div id="valor">
+            <h2>Valores</h2>
+            Hemos identificado un conjunto de valores nucleares que ayudarán a reavivar la llama de nuestro negocio. 
+            Algunos de estos valores son perdurables, surgen de nuestra historia, ya los conocemos. Otros, en cambio, 
+            nos resultarán nuevos. Pero todos ellos deben convivir en nuestros corazones y en nuestras mentes y deben 
+            manifestarse en nuestras acciones.
+        </div>            
+        
+        <div id="lista">
+            <h2>Lista de valores</h2>
+            <p>Integridad: Ser auténticos.
+                <p>
+                Calidad: Lo que hacemos, lo hacemos bien.
+                <p>    
+                Responsabilidad: Que suceda depende de uno mismo.
+                <p>    
+                Liderazgo: El coraje de forjar un futuro mejor.
+                <p>    
+                Colaboración: Potenciar el talento colectivo.
+                <p>    
+                Diversidad: Tan incluyente como nuestras marcas.
+                <p>   
+                Pasión: Comprometidos con el corazón y con la razón.
+            </p>
+        </div>
+</div>
+
+        <div id="sidebar1">
+
+            <h3>Misión</h3>
+            <p>Refrescar al mundo en cuerpo, mente y espíritu.<p>
+            Inspirar momentos de optimismo a través de nuestras marcas y acciones, 
+            para crear valor y dejar nuestra huella en cada uno de los lugares en los que operamos.</p>
+
+        </div>
+
+        <div id="sidebar2">
+
+            <h3>Visión</h3>
+            <p>Utilidades: Maximizar el retorno a los accionistas, sin perder de vista la totalidad de nuestras responsabilidades.
+                <p>
+                Gente: Ser un excelente lugar para trabajar, en donde nuestro personal se inspire para dar lo mejor de sí.
+                <p>   
+                Portafolio de Productos: Ofrecer al mundo una cartera de marcas de bebidas que se anticipan y satisfacen los deseos y las necesidades de las personas.
+                <p>   
+                Socios: Formar una red de socios exitosa y crear lealtad mutua.
+                <p>    
+                Planeta: Ser un ciudadano global, responsable, que hace su aporte para un mundo mejor.
+            </p>
+
+        </div>
+<center>
+        <div id="footer">
+                <img id="botella1" src="images/botella.png">
+                <img id="botella2" src="images/botella.png">
+        <p>
+            Contactenos:
+            <p>
+                ATENCIÓN CLIENTE – 01800 704 4400
+            <p>
+            consumidor@coca-cola.com
+            <p>
+                Sebastian Correa Vergara
+            <p>
+                Analisis y Desarrollo de Software
+        </p>
+        
+        </div>
+</center>
+
+</div>
+
+        
+    </body>
+</html>
